bcf79687f04ac12885981c62f3135fc393c0b752
charuzain <charuabhizain@gmail.com>
2022-01-01 17:18:46 -0500

    I/O in ruby

---
+++ input_output_ruby.rb
@@ -0,0 +1,9 @@
+print "What is your name? "
+name = gets.chomp
+# puts "Welcome #{name}!!"
+
+print "What is your age? "
+age = gets.chomp
+# puts "#{age} years"
+
+puts "My name is #{name} and I'm  #{age} years old !!"
